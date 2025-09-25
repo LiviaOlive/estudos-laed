70a059ecd1ffd9962a57af7b32313b9bfe8719bd
Lívia Oliveira <liviaolive100@gmail.com>
2025-09-25 16:34:54 -0300

    README e gitignore adicionados

--- revisão_ponteiros.c
+++ revisão_ponteiros.c
@@ -4,5 +4,27 @@
 int main() {
     int x = 10;
     printf("o valor de x e: %d\n", x);
+    int IDADE = 25;
+    int *PIDADE = 0;
+    PIDADE = &IDADE;
+    printf("o valor de IDADE e: %d\n", *PIDADE);
+    printf("o endereco de IDADE e: %i\n", &IDADE);
+    printf("o endereco de memoria e: %x\n", PIDADE);
+// Operador unário & (“endereço de”)
+// Operador unário * (“conteúdo de”)
+    return 0;
+}
+
+void func(){
+    int* ptr= (int*)malloc(sizeof(int));
+    *ptr = 20;
+    free(ptr);//libera a memoria alocada
+}
+
+int variaveis(){
+    int i;
+    char c;
+    float f;
+    double d;
     return 0;
 }
