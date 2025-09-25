6fa27e99dbe620906fed37cbc275af0488d76fa6
Lívia Oliveira <liviaolive100@gmail.com>
2025-09-25 16:57:34 -0300

    func impressao

--- revisão_ponteiros.c
+++ revisão_ponteiros.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void impressao(){
+    int x = 10, y = 20;
+    int *p;
+    p = &x;
+    y =*p;
+    printf("y = %d\n", y);
+    return;
+}
+
 int main() {
     int x = 10;
     printf("o valor de x e: %d\n", x);
@@ -10,6 +19,7 @@ int main() {
     printf("o valor de IDADE e: %d\n", *PIDADE);
     printf("o endereco de IDADE e: %i\n", &IDADE);
     printf("o endereco de memoria e: %x\n", PIDADE);
+    impressao();
 // Operador unário & (“endereço de”)
 // Operador unário * (“conteúdo de”)
     return 0;
@@ -28,3 +38,4 @@ int variaveis(){
     double d;
     return 0;
 }
+
